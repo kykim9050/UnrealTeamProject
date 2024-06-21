80f608a47a42a418b03b74631fe2672094529eb2
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-06-21 12:02:44 +0900

    김경윤 : Test용 Lobby, Ending, Play, Title Level 추가 및 게임모드 설정 완료

---
+++ Source/Unreal5_Portfolio/TestLevel/TestTitleMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "TestTitleMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API ATestTitleMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
