c916ef2037cd6537b2dc7b617de5d5d6a0bc7b65
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-06-24 22:41:53 +0900

    김경윤 : ContentsEnum, ContentsStruct 클래스 추가

---
+++ Source/Unreal5_Portfolio/Global/ContentsEnum.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ContentsEnum.generated.h"
+
+
+// Contents 전반적으로 사용하는 Enum class
+
+UCLASS()
+class UNREAL5_PORTFOLIO_API UContentsEnum : public UObject
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Unreal5_Portfolio/Global/ContentsStruct.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ContentsStruct.generated.h"
+
+
+// Contents 전반적으로 사용하는 Struct
+
+UCLASS()
+class UNREAL5_PORTFOLIO_API UContentsStruct : public UObject
+{
+	GENERATED_BODY()
+	
+};
