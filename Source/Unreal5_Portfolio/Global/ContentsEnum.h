4830fb68443c3063fc9d0fd44ff637ef65abad60
김수원 <rla5233@naver.com>
2024-06-26 14:50:52 +0900

    김수원 : Monster 업데이트

--- Source/Unreal5_Portfolio/Global/ContentsEnum.h
+++ Source/Unreal5_Portfolio/Global/ContentsEnum.h
@@ -8,6 +8,25 @@
 
 
 // Contents 전반적으로 사용하는 Enum class
+UENUM(BlueprintType)
+enum class EMonsterState : uint8
+{
+	Idle,
+	Patrol,
+	Chase,
+	Attack,
+	Dead
+};
+
+UENUM(BlueprintType)
+enum class EZombie1Anim : uint8
+{
+	Idle,
+	Walk,
+	Run,
+	Attack,
+	Dead
+};
 
 UCLASS()
 class UNREAL5_PORTFOLIO_API UContentsEnum : public UObject
