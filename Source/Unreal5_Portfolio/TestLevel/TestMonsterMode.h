b575d507a419cea7762f3a10c362e5ba245a298f
김수원 <rla5233@naver.com>
2024-06-26 12:18:49 +0900

    김수원 : Monster 테스트 환경 추가

---
+++ Source/Unreal5_Portfolio/TestLevel/TestMonsterMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "TestMonsterMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API ATestMonsterMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
