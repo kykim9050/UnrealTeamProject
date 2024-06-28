14bfef02bb5bf64326a92cc342ea676cb5070114
kjb <kjb_jh@naver.com>
2024-06-28 18:51:04 +0900

    김종범 : 애니메이션 Instance 추가.

---
+++ Source/Unreal5_Portfolio/Global/MainAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UMainAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
