4411f6344b4059bb180d921f0d760483b01a5fc2
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-06-28 17:00:20 +0900

    김경윤 : 데이터 에셋 추가

---
+++ Source/Unreal5_Portfolio/Global/DataAssets/InputDatas.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "InputDatas.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UInputDatas : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+
+protected:
+
+private:
+
+};
