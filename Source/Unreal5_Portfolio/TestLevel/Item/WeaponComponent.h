af024241d0070af93a49aa7b4d8d37c0a3607659
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-06-30 22:09:05 +0900

    김경윤 : 아이템 Test 관련 클래스 추가

---
+++ Source/Unreal5_Portfolio/TestLevel/Item/WeaponComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "WeaponComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UWeaponComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
