36c0ef3a513fc8014efd616ae00f1ff665a7c487
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-07-01 14:01:50 +0900

    김경윤 : 파트별 개발용 레벨 제공

---
+++ Source/Unreal5_Portfolio/PartDevLevel/Object/ObjectDevMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ObjectDevMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API AObjectDevMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
