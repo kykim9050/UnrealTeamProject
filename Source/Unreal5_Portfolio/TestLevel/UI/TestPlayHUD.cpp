29b9e45b1d243fdd701b0d0d57270737f71aafff
yndoo 배연두 <yndoo2k@gmail.com>
2024-07-03 17:40:09 +0900

    배연두 : TestPlayerUI 인스턴스 Get 함수 수정

--- Source/Unreal5_Portfolio/TestLevel/UI/TestPlayHUD.cpp
+++ Source/Unreal5_Portfolio/TestLevel/UI/TestPlayHUD.cpp
@@ -2,4 +2,23 @@
 
 
 #include "TestLevel/UI/TestPlayHUD.h"
+#include "Global/MainGameBlueprintFunctionLibrary.h"
 
+void ATestPlayHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//UMainGameInstance* Inst = UMainGameBlueprintFunctionLibrary::GetMainGameInstance(GetWorld());
+
+	//TMap<FString, TSubclassOf<UUserWidget>>& AllUI = Inst->GetTPSPlayWidgets();
+
+	//// 애초에 빌드할때는 
+	//for (TPair<FString, TSubclassOf<UUserWidget>> Pair : AllUI)
+	//{
+	//	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), Pair.Value);
+	//	Widget->AddToViewport();
+	//	Widgets.Add(Pair.Key, Widget);
+	//}
+
+
+}
