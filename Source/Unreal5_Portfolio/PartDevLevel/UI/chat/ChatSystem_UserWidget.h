cf642b7a37dd8d52344274a1593609287ca06fcf
조선욱 <gs.8life@gmail.com>
2024-07-04 16:14:58 +0900

    조선욱 : c++클래스 생성

---
+++ Source/Unreal5_Portfolio/PartDevLevel/UI/chat/ChatSystem_UserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ChatSystem_UserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UChatSystem_UserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
