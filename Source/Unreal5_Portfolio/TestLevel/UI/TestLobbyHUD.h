16f99df02f16c9166ac5fc8f26e305d6dcb5ab46
yndoo 배연두 <yndoo2k@gmail.com>
2024-07-04 18:42:02 +0900

    배연두 : Test UI들 인풋모드 에러 해결

---
+++ Source/Unreal5_Portfolio/TestLevel/UI/TestLobbyHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TestLobbyHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API ATestLobbyHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
