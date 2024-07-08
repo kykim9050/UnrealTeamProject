b572a6c9dbe177cbc8d4ffe3d0db30f9188c2923
yndoo 배연두 <yndoo2k@gmail.com>
2024-07-08 18:41:13 +0900

    배연두 : Test미니맵 수정

--- Source/Unreal5_Portfolio/PartDevLevel/UI/InGame/MinimapCaptureCamera.cpp
+++ Source/Unreal5_Portfolio/PartDevLevel/UI/InGame/MinimapCaptureCamera.cpp
@@ -28,8 +28,6 @@ void AMinimapCaptureCamera::Tick(float DeltaTime)
 	ATestCharacter* MyCharacter = Cast<ATestCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
 	
 	FVector CharacterPos = MyCharacter->GetActorLocation();
-	CharacterPos.Z = -8000;
-
 	SetActorLocation(CharacterPos);
 }
 
