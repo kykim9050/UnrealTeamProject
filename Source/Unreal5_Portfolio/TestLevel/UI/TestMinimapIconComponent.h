a8ab11fefe0651a1cf6d43388ad33f0dde028572
yndoo 배연두 <yndoo2k@gmail.com>
2024-07-09 15:08:07 +0900

    배연두 : 미니맵 플레이어 아이콘 컴포넌트 추가

---
+++ Source/Unreal5_Portfolio/TestLevel/UI/TestMinimapIconComponent.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperSpriteComponent.h"
+#include "TestMinimapIconComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ShowCategories = (Mobility), ClassGroup = Paper2D, meta = (BlueprintSpawnableComponent))
+class UNREAL5_PORTFOLIO_API UTestMinimapIconComponent : public UPaperSpriteComponent
+{
+	GENERATED_BODY()
+	UTestMinimapIconComponent();
+	void BeginPlay() override;
+};
