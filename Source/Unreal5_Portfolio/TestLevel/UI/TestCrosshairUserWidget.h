033f20b4f6451e18acba031b0d18e11668fe19c7
정우 <choeun1999@gmail.com>
2024-07-11 18:51:15 +0900

    조정우 : 테스트 커밋

---
+++ Source/Unreal5_Portfolio/TestLevel/UI/TestCrosshairUserWidget.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TestCrosshairUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UTestCrosshairUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	//void NativeConstruct();
+	//void NativeTick(const FGeometry& _MyGeometry, float _InDeltaTime);
+
+};
