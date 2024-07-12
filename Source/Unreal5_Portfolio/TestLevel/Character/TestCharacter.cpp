88eb4344fb97d87316bdb8da8c929b73a04c4d7f
yoon-park <jiyoonpark.dev@gmail.com>
2024-07-12 16:21:19 +0900

    박지윤 : 캐릭터 아이템 장착 방식 수정
    - StaticMeshComponent를 세팅하는 방식 대신, AActor를 attach하는 방식으로 변경

--- Source/Unreal5_Portfolio/TestLevel/Character/TestCharacter.cpp
+++ Source/Unreal5_Portfolio/TestLevel/Character/TestCharacter.cpp
@@ -4,6 +4,7 @@
 #include "TestCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Engine/SkeletalMeshSocket.h"
 #include "Global/MainGameBlueprintFunctionLibrary.h"
 #include "Global/DataTable/ItemDataRow.h"
 #include "Components/SphereComponent.h"
@@ -41,6 +42,7 @@ ATestCharacter::ATestCharacter()
 	UEnum* Enum = StaticEnum<EPlayerPosture>();
 	for (size_t i = 0; i < static_cast<size_t>(EPlayerPosture::Barehand); i++)
 	{
+		/*
 		// Weapon Meshes
 		FString Name = Enum->GetNameStringByValue(i) + "Socket";
 		UStaticMeshComponent* NewSocketMesh = CreateDefaultSubobject<UStaticMeshComponent>(*Name);
@@ -50,6 +52,7 @@ ATestCharacter::ATestCharacter()
 		NewSocketMesh->SetVisibility(false);
 		NewSocketMesh->SetIsReplicated(true);
 		ItemMeshes.Push(NewSocketMesh);
+		*/
 
 		// Inventory (for UI Test)
 		FItemInformation NewSlot;
@@ -287,11 +290,12 @@ void ATestCharacter::ChangePosture_Implementation(EPlayerPosture _Type)
 	{
 		PostureValue = _Type;
 		CurItemIndex = -1;
-
+		/*
 		for (size_t i = 0; i < static_cast<size_t>(EPlayerPosture::Barehand); i++)
 		{
 			ItemMeshes[i]->SetVisibility(false);
 		}
+		*/
 	}
 	else
 	{
@@ -304,7 +308,7 @@ void ATestCharacter::ChangePosture_Implementation(EPlayerPosture _Type)
 
 		PostureValue = _Type;
 		CurItemIndex = ItemSlotIndex;
-
+		/*
 		for (size_t i = 0; i < static_cast<size_t>(EPlayerPosture::Barehand); i++)
 		{
 			if (i == static_cast<size_t>(_Type))
@@ -316,6 +320,7 @@ void ATestCharacter::ChangePosture_Implementation(EPlayerPosture _Type)
 				ItemMeshes[i]->SetVisibility(false);
 			}
 		}
+		*/
 	}
 }
 
@@ -360,9 +365,15 @@ void ATestCharacter::PickUpItem_Implementation()
 
 	uint8 ItemIndex = static_cast<uint8>(ItemType); // 사용할 소켓 번호.
 
+	// Attaching Item
+	const USkeletalMeshSocket* WeaponSocket = GetMesh()->GetSocketByName("ItemSocket");
+	WeaponSocket->AttachActor(GetMapItem, GetMesh());
+
+	/*
 	// Setting Weapon Mesh
 	ItemMeshes[ItemIndex]->SetStaticMesh(ItemMesh); // Static Mesh 적용.
 	GetMapItem->Destroy(); // Map에 있는 아이템 삭제.
+	*/
 
 	// Setting Inventory
 	ItemSlot[ItemIndex].Name = ItemStringToName;
