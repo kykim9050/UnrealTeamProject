87d21eab763d2f455439e74f910944cd672f5596
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-07-12 15:59:14 +0900

    김경윤 : 데이터 테이블 파일 리스트 추출

--- Source/Unreal5_PortfolioEditor/DataEditor.cpp
+++ Source/Unreal5_PortfolioEditor/DataEditor.cpp
@@ -3,7 +3,8 @@
 
 #include "DataEditor.h"
 #include "Components/DetailsView.h"
-#include "Global/ContentsLog.h"
+#include "Unreal5_Portfolio/Global/ContentsLog.h"
+//#include "Unreal5_Portfolio/Global/MainGameBlueprintFunctionLibrary.h"
 
 UDataEditor::UDataEditor()
 {
@@ -23,10 +24,30 @@ void UDataEditor::NativeConstruct()
 		return;
 	}
 
+	TArray<FString> FileList = TArray<FString>();
+
+	FString Path = FPaths::ProjectContentDir();
+	FString DataTablePath = "Resources/Global/DataTable";
+	Path = Path + DataTablePath + TEXT("/*.*");
+
+	IFileManager::Get().FindFiles(FileList, *Path, true, false);
+
+	//UMainGameInstance* Inst = UMainGameBlueprintFunctionLibrary::GetMainGameInstance(GetWorld());
+
+	//if (nullptr == Inst)
+	//{
+	//	UE_LOG(UILog, Fatal, TEXT("%s(%u)> if (nullptr == DebugText)"), __FUNCTION__, __LINE__);
+	//	return;
+	//}
+
+	for (size_t i = 0; i < FileList.Num(); i++)
+	{
+		//TSubclassOf<UDataTable> DataTable =
+		//	DataTables.Add(DataTable);
+	}
+
 	FString ItemStr = "DataTables";
 	FName ItemName = FName(ItemStr);
 	DetailView->PropertiesToShow.Add(ItemName);
 	DetailView->Modify();
-
-	int a = 0;
 }
