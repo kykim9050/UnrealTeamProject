4b831b937603e3625f9fcd69ff40c0c8209d7258
김수원 <rla5233@naver.com>
2024-07-18 12:37:29 +0900

    김수원 : 메인 레벨 몬스터 TaskNode 추가

--- Source/Unreal5_Portfolio/PartDevLevel/Monster/AI/BTTaskNode_MonsterAttack.cpp
+++ Source/Unreal5_Portfolio/PartDevLevel/Monster/AI/BTTaskNode_MonsterAttack.cpp
@@ -23,7 +23,6 @@ EBTNodeResult::Type UBTTaskNode_MonsterAttack::ExecuteTask(UBehaviorTreeComponen
 	}
 
 	UMonsterData* MonsterData = GetValueAsObject<UMonsterData>(_OwnerComp, TEXT("MonsterData"));
-	MonsterData->IdleTime = 0.0f;
 	Monster->ChangeAniValue(ETestMonsterAnim::Attack);
 	MonsterData->AttackTime = Monster->GetAnimInstance()->GetKeyAnimMontage(static_cast<uint8>(ETestMonsterAnim::Attack))->GetPlayLength();
 
