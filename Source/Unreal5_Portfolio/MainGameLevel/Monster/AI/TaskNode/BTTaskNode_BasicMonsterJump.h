2560417580eede316544a7af1a3d483d37d48151
김수원 <rla5233@naver.com>
2024-07-18 14:52:26 +0900

    김수원 : 메인 레벨 몬스터 TaskNode 추가

---
+++ Source/Unreal5_Portfolio/MainGameLevel/Monster/AI/TaskNode/BTTaskNode_BasicMonsterJump.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainGameLevel/Monster/AI/TaskNode/BTTaskNodeBase_BasicMonster.h"
+#include "BTTaskNode_BasicMonsterJump.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UBTTaskNode_BasicMonsterJump : public UBTTaskNodeBase_BasicMonster
+{
+	GENERATED_BODY()
+
+public:
+	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* pNodeMemory, float DeltaSeconds) override;
+};
