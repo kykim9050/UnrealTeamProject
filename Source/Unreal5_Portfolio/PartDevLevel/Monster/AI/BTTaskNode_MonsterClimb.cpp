0531d4f78cd76ce589b64ba40aca2bed07aca1bb
정우 <choeun1999@gmail.com>
2024-07-18 19:06:15 +0900

    조정우 : 몬스터 CLimb 수정중

--- Source/Unreal5_Portfolio/PartDevLevel/Monster/AI/BTTaskNode_MonsterClimb.cpp
+++ Source/Unreal5_Portfolio/PartDevLevel/Monster/AI/BTTaskNode_MonsterClimb.cpp
@@ -42,8 +42,9 @@ void UBTTaskNode_MonsterClimb::TickTask(UBehaviorTreeComponent& _OwnerComp, uint
 	FVector CurPos = Monster->GetActorLocation();
 	FVector ForwardVector = Monster->GetActorForwardVector();
 	
-	if (CurPos.Z >= Dest.Z - Monster->GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight())
+	if (CurPos.Z >= Dest.Z - (Monster->GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight() + 45.0f))
 	{
+		Monster->GetCapsuleComponent()->SetCapsuleRadius(34.0f);
 		Monster->GetCharacterMovement()->MovementMode = EMovementMode::MOVE_None;
 		StateChange(_OwnerComp, EMonsterState::ClimbEnd);
 		return;
--- Source/Unreal5_Portfolio/PartDevLevel/Monster/AI/BTTaskNode_Monster_ClimbEnd.cpp
+++ Source/Unreal5_Portfolio/PartDevLevel/Monster/AI/BTTaskNode_Monster_ClimbEnd.cpp
@@ -7,12 +7,14 @@
 #include "PartDevLevel/Monster/TestMonsterBase.h"
 #include "Global/ContentsLog.h"
 
-#include "GameFramework/CharacterMovementComponent.h"
 #include "Navigation/PathFollowingComponent.h"
 #include "NavigationSystem.h"
 #include "NavigationData.h"
 #include "Global/Animation/MainAnimInstance.h"
 #include "Components/CapsuleComponent.h"
+#include "Engine/SkeletalMeshSocket.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 EBTNodeResult::Type UBTTaskNode_Monster_ClimbEnd::ExecuteTask(UBehaviorTreeComponent& _OwnerComp, uint8* _NodeMemory)
 {
@@ -24,10 +26,11 @@ EBTNodeResult::Type UBTTaskNode_Monster_ClimbEnd::ExecuteTask(UBehaviorTreeCompo
 		UE_LOG(MonsterLog, Fatal, TEXT("Monster Is Not Valid"));
 		return EBTNodeResult::Aborted;
 	}
+
 	Monster->ChangeAniValue(EMonsterAnim::ClimbEnd);
 	UMonsterData* MonsterData = GetValueAsObject<UMonsterData>(_OwnerComp, TEXT("MonsterData"));
 	MonsterData->ClimbTime = Monster->GetAnimInstance()->GetKeyAnimMontage(static_cast<uint8>(EMonsterAnim::ClimbEnd))->GetPlayLength();
-
+	Monster->SetActorLocation(Monster->GetActorLocation() + Monster->GetActorForwardVector() * 10.0f);
 	return EBTNodeResult::InProgress;
 }
 
@@ -37,32 +40,28 @@ void UBTTaskNode_Monster_ClimbEnd::TickTask(UBehaviorTreeComponent& _OwnerComp, 
 
 	UMonsterData* MonsterData = GetValueAsObject<UMonsterData>(_OwnerComp, TEXT("MonsterData"));
 	ATestMonsterBase* Monster = GetActor<ATestMonsterBase>(_OwnerComp);
+	if (nullptr == Monster)
+	{
+		UE_LOG(MonsterLog, Fatal, TEXT("Monster Is Null"));
+		return;
+	}
+
 	if(0.0f >= MonsterData->ClimbTime)
 	{
-		ATestMonsterBaseAIController* Controller = Cast<ATestMonsterBaseAIController>(Monster->GetController());
-		FVector Dest = Controller->GetBlackboardComponent()->GetValueAsVector("DestinationLocation");
-		//Dest.Z = 0.0f;
-		FVector CurPos = Monster->GetActorLocation();
-		//CurPos.Z = 0.0f;
-
-		FVector DirVector = (Dest - CurPos);
-		DirVector.Normalize();
-		if (abs(CurPos.X - Dest.X) >= 50.0f || abs(CurPos.Y - Dest.Y) >= 50.0f)
-		{
-			Monster->SetActorLocation(CurPos + DirVector * 50.0f);
-		}
-		else
-		{
-			Dest.Z += Monster->GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight();
-			Monster->GetCapsuleComponent()->SetCapsuleRadius(34.0f);
-			Monster->SetActorRelativeLocation(Dest);
-			Monster->GetCharacterMovement()->SetMovementMode(MOVE_NavWalking);
-			StateChange(_OwnerComp, EMonsterState::Chase);
-			return;
-		}
+		/*FVector LeftFootLoc = Monster->GetMesh()->GetBoneLocation("Base-HumanLLegDigit11", EBoneSpaces::WorldSpace);
+		LeftFootLoc.Z += (Monster->GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight());
+		Monster->SetActorLocation(LeftFootLoc);
+		Monster->GetCharacterMovement()->SetMovementMode(MOVE_NavWalking);*/
+		StateChange(_OwnerComp, EMonsterState::Chase);
+		return;
 	}
-	else
+	/*else
 	{
-		MonsterData->ClimbTime -= _DeltaSeconds;
-	}
+		FVector FirstPos = Monster->GetActorLocation();
+		float ClimbDist = Monster->GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight() * 2.0f;
+		FVector Result = FirstPos;
+		Result.Z += ClimbDist;
+
+		Monster->AddActorLocalOffset()
+	}*/
 }
