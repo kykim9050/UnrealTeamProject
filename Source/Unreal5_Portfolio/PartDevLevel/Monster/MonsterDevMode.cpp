45add23d03cbd190b811d9cc3ebf8207ed8fc533
김수원 <rla5233@naver.com>
2024-07-22 17:18:38 +0900

    김수원 : 몬스터 테스트 환경 수정

--- Source/Unreal5_Portfolio/PartDevLevel/Monster/MonsterDevMode.cpp
+++ Source/Unreal5_Portfolio/PartDevLevel/Monster/MonsterDevMode.cpp
@@ -10,17 +10,6 @@
 void AMonsterDevMode::BeginPlay()
 {
 	Super::BeginPlay();
-
-	for (int i = 0; i < 1; i++)
-	{
-		FString MonsterType = "MonsterType_1";
-		UMainGameInstance* MainGameInst = UMainGameBlueprintFunctionLibrary::GetMainGameInstance(GetWorld());
-		const FMonsterDataRow* MonsterData = MainGameInst->GetMonsterData(FName(MonsterType));
-
-		FTransform Transform;
-		Transform.SetLocation(FVector(-270.0f + i * 100.0f, -40.0f, 298.0f));
-		GetWorld()->SpawnActor<AActor>(MonsterData->GetMonsterUClass(), Transform);
-	}
 }
 
 void AMonsterDevMode::PostLogin(APlayerController* NewPlayer)
