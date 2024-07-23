c78f41e533dc9630f6a6e29235866810321e53f4
ChoiYeongHwan <cyh5343@naver.com>
2024-07-23 18:06:04 +0900

    최영환 : boss 총알 충돌 및 자동삭제 추가

--- Source/Unreal5_Portfolio/PartDevLevel/Monster/Boss/BossProjectile.h
+++ Source/Unreal5_Portfolio/PartDevLevel/Monster/Boss/BossProjectile.h
@@ -26,7 +26,7 @@ public:
 	void FireInDirection(const FVector& ShootDirection);
 
 	UFUNCTION()
-	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+	void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 private:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
@@ -37,5 +37,9 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Movement")
 	class UProjectileMovementComponent* ProjectileMovement;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	float ProjectileCollisionRadius;
 
 };
