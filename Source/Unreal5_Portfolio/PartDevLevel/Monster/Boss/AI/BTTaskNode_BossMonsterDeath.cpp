90d634df3cde0dd5cd89309dd38ad3bae1f740ab
ChoiYeongHwan <cyh5343@naver.com>
2024-07-24 16:55:41 +0900

    최영환 : Boss BT Death 구현

--- Source/Unreal5_Portfolio/PartDevLevel/Monster/Boss/AI/BTTaskNode_BossMonsterDeath.cpp
+++ Source/Unreal5_Portfolio/PartDevLevel/Monster/Boss/AI/BTTaskNode_BossMonsterDeath.cpp
@@ -2,4 +2,36 @@
 
 
 #include "PartDevLevel/Monster/Boss/AI/BTTaskNode_BossMonsterDeath.h"
+#include "PartDevLevel/Monster/Boss/TestBossMonsterBase.h"
 
+#include "Global/DataTable/BossDataRow.h"
+#include "Global/ContentsLog.h"
+
+EBTNodeResult::Type UBTTaskNode_BossMonsterDeath::ExecuteTask(UBehaviorTreeComponent& _OwnerComp, uint8* _NodeMemory)
+{
+	Super::ExecuteTask(_OwnerComp, _NodeMemory);
+
+	return EBTNodeResult::Type::InProgress;
+}
+
+void UBTTaskNode_BossMonsterDeath::TickTask(UBehaviorTreeComponent& _OwnerComp, uint8* _pNodeMemory, float _DeltaSeconds)
+{
+	Super::TickTask(_OwnerComp, _pNodeMemory, _DeltaSeconds);
+
+	ATestBossMonsterBase* BossMonster = GetActor<ATestBossMonsterBase>(_OwnerComp);
+	if (false == BossMonster->IsValidLowLevel())
+	{
+		LOG(MonsterLog, Fatal, TEXT("BossMonster Is Not Valid"));
+		return;
+	}
+
+	UBossData* BossData = GetValueAsObject<UBossData>(_OwnerComp, TEXT("BossMonsterData"));
+
+	if (0 >= BossData->Data->GetHP())
+	{
+		StateChange(_OwnerComp, EBossMonsterState::Dead);
+		BossMonster->ChangeAniValue(EBossMonsterAnim::Dead);
+		BossMonster->SetLifeSpan(5.0f);
+		return;
+	}
+}
