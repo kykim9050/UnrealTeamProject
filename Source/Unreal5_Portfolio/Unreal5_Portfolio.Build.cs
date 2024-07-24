c3ddbb5d8de605a7b4749e9a4063bf87c691f7f2
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-07-24 19:11:04 +0900

    김경윤 : motion warping 모듈 & 플러그인 추가

--- Source/Unreal5_Portfolio/Unreal5_Portfolio.Build.cs
+++ Source/Unreal5_Portfolio/Unreal5_Portfolio.Build.cs
@@ -10,7 +10,7 @@ public class Unreal5_Portfolio : ModuleRules
 
 		PublicIncludePaths.Add(System.IO.Path.Combine(ModuleDirectory, ""));
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule" , "EnhancedInput", "Paper2D"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule" , "EnhancedInput", "Paper2D", "MotionWarping" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
