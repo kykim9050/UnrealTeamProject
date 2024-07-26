139deaed381740a776c8d93362418570917c3587
yoon-park <jiyoonpark.dev@gmail.com>
2024-07-26 16:02:56 +0900

    박지윤 : 아이템 수정
    - replicated 여부 수정

--- Source/Unreal5_Portfolio/MainGameLevel/Object/ItemBase.cpp
+++ Source/Unreal5_Portfolio/MainGameLevel/Object/ItemBase.cpp
@@ -9,12 +9,13 @@ AItemBase::AItemBase()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
 	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
 	SetRootComponent(CapsuleComponent);
 
 	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
 	StaticMeshComponent->SetupAttachment(RootComponent);
+	StaticMeshComponent->SetIsReplicated(true);
 }
 
 // Called when the game starts or when spawned
