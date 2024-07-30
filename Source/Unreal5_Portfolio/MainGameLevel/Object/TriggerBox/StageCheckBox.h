567a0760b8376d601c6ad9cedf6fcca17c95c9c3
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-07-30 17:51:38 +0900

    김경윤 : Player 체크용 StageCheckBox 클래스 추가

---
+++ Source/Unreal5_Portfolio/MainGameLevel/Object/TriggerBox/StageCheckBox.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "StageCheckBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API AStageCheckBox : public ATriggerBox
+{
+	GENERATED_BODY()
+
+public:
+	AStageCheckBox();
+
+protected:
+
+	UFUNCTION()
+	void OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor);
+
+	UFUNCTION()
+	void OnOverlapEnd(AActor* OverlappedActor, AActor* OtherActor);
+
+private:
+
+};
