951c2d7a086e04096f2db9c6dae7d9ad8c891f45
kjb <kjb_jh@naver.com>
2024-07-30 19:02:01 +0900

    김종범 : 노즐 파이클 전용 노티파이 Class 추가.

---
+++ Source/Unreal5_Portfolio/MainGameLevel/Player/AnimNotifyState_Muzzle_Particle.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainGameLevel/Player/AnimNotifyState_Muzzle_Particle.h"
+#include "TestLevel/Character/TestCharacter.h"
+#include "MainGameLevel/Player/MainCharacter.h"
+
+void UAnimNotifyState_Muzzle_Particle::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	ATestCharacter* PlayCharacter = MeshComp->GetOwner<ATestCharacter>();
+	if (nullptr == PlayCharacter)
+	{
+		return;
+	}
+	PlayCharacter->ShowMuzzle();
+}
+
+void UAnimNotifyState_Muzzle_Particle::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+}
+
+void UAnimNotifyState_Muzzle_Particle::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+}
---
+++ Source/Unreal5_Portfolio/MainGameLevel/Player/AnimNotifyState_Muzzle_Particle.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_Muzzle_Particle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UAnimNotifyState_Muzzle_Particle : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+public :
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
