71bcb7440ce1377223ed3248da1cdf527549092e
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-07-31 23:32:31 +0900

    김경윤 : CollisionBegin, End 함수 바인딩

--- Source/Unreal5_Portfolio/MainGameLevel/Object/ReportObject.cpp
+++ Source/Unreal5_Portfolio/MainGameLevel/Object/ReportObject.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MainGameLevel/Object/ReportObject.h"
 #include "Components/CapsuleComponent.h"
 
@@ -18,10 +17,22 @@ void AReportObject::BeginPlay()
 	GetCollisionComponent()->SetCollisionProfileName(MainColName);
 
 	SetInfo(Name);
-	
+
+	GetCollisionComponent()->OnComponentBeginOverlap.AddDynamic(this, &AReportObject::OnCollisionOverlapBegin);
+	GetCollisionComponent()->OnComponentEndOverlap.AddDynamic(this, &AReportObject::OnCollisionOverlapEnd);
 }
 
 void AReportObject::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AReportObject::OnCollisionOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	int a = 0;
+}
+
+void AReportObject::OnCollisionOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	int a = 0;
 }
--- Source/Unreal5_Portfolio/MainGameLevel/Object/ReportObject.h
+++ Source/Unreal5_Portfolio/MainGameLevel/Object/ReportObject.h
@@ -15,11 +15,17 @@ class UNREAL5_PORTFOLIO_API AReportObject : public AMapObjectBase
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION()
+	void OnCollisionOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnCollisionOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 
 protected:
 	void BeginPlay() override;
 	void Tick(float DeltaTime) override;
 
 private:
-
+ 
 };
