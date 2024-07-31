84c26039b30dc78258552b1d21331cf748d033d7
김수원 <rla5233@naver.com>
2024-07-31 15:07:00 +0900

    김수원 : 테스트 Mutant 생성 및 리소스 수정

---
+++ Source/Unreal5_Portfolio/PartDevLevel/Monster/NonBoss/Mutant/TestMutant.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PartDevLevel/Monster/Base/TestMonsterBase.h"
+#include "TestMutant.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API ATestMutant : public ATestMonsterBase
+{
+	GENERATED_BODY()
+	
+};
