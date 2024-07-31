cd907c2bee34835e47b93b07135b6035e41f52d0
yndoo 배연두 <yndoo2k@gmail.com>
2024-07-31 17:25:59 +0900

    배연두 : 메인로비 캐릭터 선택 UI 추가

---
+++ Source/Unreal5_Portfolio/MainGameLevel/UI/Lobby/CharacterSelectionUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacterSelectionUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UCharacterSelectionUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
