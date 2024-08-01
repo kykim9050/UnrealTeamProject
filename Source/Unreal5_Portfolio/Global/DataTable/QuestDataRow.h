54af27d28fb35fb577bf282f5bc2c05436f2cfc4
gs.8life <gs.8life@gmail.com>
2024-08-01 14:32:50 +0900

    조선욱 : 퀘스트 업데이트

--- Source/Unreal5_Portfolio/Global/DataTable/QuestDataRow.h
+++ Source/Unreal5_Portfolio/Global/DataTable/QuestDataRow.h
@@ -14,30 +14,15 @@ struct FQuestDataRow : public FTableRowBase
 {
 	GENERATED_BODY()
 	
-public:
-
-	FORCEINLINE FVector GetQuestLocation() const
-	{
-		return QuestLocation;
-	}
-
-	FORCEINLINE FString GetQuestText() const
-	{
-		return QuestText;
-	}
-
-protected:
-
-private:
 	/// <summary>
 	/// 퀘스트 위치 값
 	/// </summary>
-	UPROPERTY(EditAnywhere, meta = (AllowprivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowprivateAccess = "true"))
 	FVector QuestLocation = FVector(0.0f, 0.0f, 0.0f);
 
 	/// <summary>
 	/// 퀘스트 내용
 	/// </summary>
-	UPROPERTY(EditAnywhere, meta = (AllowprivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowprivateAccess = "true"))
 	FString QuestText = "";
 };
