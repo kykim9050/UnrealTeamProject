7452cc0906500a8c54e9a69cb1698e8b6e370e40
김수원 <rla5233@naver.com>
2024-08-01 12:34:02 +0900

    김수원 : [Main] Mutant 클래스 생성

---
+++ Source/Unreal5_Portfolio/MainGameLevel/Monster/BasicMonster/Mutant/BasicMutant.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainGameLevel/Monster/Base/BasicMonsterBase.h"
+#include "BasicMutant.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API ABasicMutant : public ABasicMonsterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Unreal5_Portfolio/MainGameLevel/Monster/Data/BasicMutantData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainGameLevel/Monster/Data/BasicMonsterData.h"
+#include "BasicMutantData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UBasicMutantData : public UBasicMonsterData
+{
+	GENERATED_BODY()
+	
+};
