63b4b687ac8106a2f674fe03efe3c753b923ae98
김수원 <rla5233@naver.com>
2024-08-02 17:36:33 +0900

    김수원 : [Main] Mutant JumpAttack 업데이트

---
+++ Source/Unreal5_Portfolio/MainGameLevel/Monster/BasicMonster/Mutant/Notify/BasicMutantJumpAttackNotify.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "BasicMutantJumpAttackNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UBasicMutantJumpAttackNotify : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
