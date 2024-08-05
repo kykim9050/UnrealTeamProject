67ac04754f6786daa5674bdf86512577f9b459bf
김수원 <rla5233@naver.com>
2024-08-05 16:07:30 +0900

    김수원 : [Main] ClimbZombie 수정

---
+++ Source/Unreal5_Portfolio/MainGameLevel/Monster/BasicMonster/ClimbZombie/ClimbZombeData.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainGameLevel/Monster/Base/BasicMonsterData.h"
+#include "ClimbZombeData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UClimbZombeData : public UBasicMonsterData
+{
+	GENERATED_BODY()
+
+public:
+	float ClimbSpeed = 50.0f;
+	
+};
