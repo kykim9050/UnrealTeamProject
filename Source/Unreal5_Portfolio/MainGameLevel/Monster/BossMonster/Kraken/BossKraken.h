07c77cd1a1c173cdfb18eae169d5851d81ffdeb7
김수원 <rla5233@naver.com>
2024-08-06 16:44:39 +0900

    김수원 : [Main] BossMonster 데이터 테이블 세팅, BossKraken 클래스 생성

---
+++ Source/Unreal5_Portfolio/MainGameLevel/Monster/BossMonster/Kraken/BossKraken.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainGameLevel/Monster/Base/BossMonsterBase.h"
+#include "BossKraken.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API ABossKraken : public ABossMonsterBase
+{
+	GENERATED_BODY()
+	
+};
