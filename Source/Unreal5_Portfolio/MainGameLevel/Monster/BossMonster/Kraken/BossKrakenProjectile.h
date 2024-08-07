57a5bcaac2af7e0079fa146eb0258f24f5a52d6c
김수원 <rla5233@naver.com>
2024-08-07 18:53:31 +0900

    김수원 : [Main] Kraken Projectile 업데이트

--- Source/Unreal5_Portfolio/MainGameLevel/Monster/BossMonster/Kraken/BossKrakenProjectile.h
+++ Source/Unreal5_Portfolio/MainGameLevel/Monster/BossMonster/Kraken/BossKrakenProjectile.h
@@ -6,9 +6,20 @@
 #include "GameFramework/Actor.h"
 #include "BossKrakenProjectile.generated.h"
 
+class UParticleSystem;
 class USphereComponent;
 class UStaticMeshComponent;
 class UProjectileMovementComponent;
+
+namespace BossKraKenProjectile
+{
+	enum class EState
+	{
+		Fly,
+		Stop,
+		Max
+	};
+}
 
 UCLASS()
 class UNREAL5_PORTFOLIO_API ABossKrakenProjectile : public AActor
@@ -24,11 +35,29 @@ protected:
 
 	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-	
+private:
+	UFUNCTION()
+	void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	void StateUpdate(float DeltaTimes);
+	void StateChange(BossKraKenProjectile::EState NextState);
+
+	void Fly(float DeltaTimes);
+	void FlyStart();
+
 private:
 	// Data
-	UPROPERTY(Replicated)
+	UPROPERTY(Replicated, EditAnywhere, Category = "SettingData", meta = (AllowPrivateAccess = true))
 	float Hp = 100.0f;
+	
+	UPROPERTY(EditAnywhere, Category = "SettingData", meta = (AllowPrivateAccess = true))
+	float Damage = 0.0f;
+
+	UPROPERTY(EditAnywhere, Category = "SettingData", meta = (AllowPrivateAccess = true))
+	float RotateSpeed = 0.0f;
+
+	BossKraKenProjectile::EState CurState = BossKraKenProjectile::EState::Max;
 
 private:
 	// Component
@@ -40,5 +69,11 @@ private:
 
 	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = true))
 	UProjectileMovementComponent* MovementComp = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Particle", meta = (AllowPrivateAccess = true))
+	UParticleSystem* FireParticle = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Particle", meta = (AllowPrivateAccess = true))
+	UParticleSystem* CrashParticle = nullptr;
 
 };
