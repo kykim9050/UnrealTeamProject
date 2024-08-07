2757a4f79891f5435b0c897a819f7401378ee7de
yndoo 배연두 <yndoo2k@gmail.com>
2024-08-07 11:44:46 +0900

    배연두 : 엔딩 레벨 추가

---
+++ Source/Unreal5_Portfolio/MainGameLevel/EndingGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "EndingGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API AEndingGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
