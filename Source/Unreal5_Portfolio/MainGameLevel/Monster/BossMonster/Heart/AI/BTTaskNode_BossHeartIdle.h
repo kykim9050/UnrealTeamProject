ce57ab72b803e716b6b3433009a4aa2897a30f1f
김수원 <rla5233@naver.com>
2024-08-08 18:52:28 +0900

    김수원 : [Main] BossHeart TaskNode 생성

---
+++ Source/Unreal5_Portfolio/MainGameLevel/Monster/BossMonster/Heart/AI/BTTaskNode_BossHeartIdle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainGameLevel/Monster/Base/BTTaskNodeBase_Monster.h"
+#include "BTTaskNode_BossHeartIdle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL5_PORTFOLIO_API UBTTaskNode_BossHeartIdle : public UBTTaskNodeBase_Monster
+{
+	GENERATED_BODY()
+	
+};
