7dff2c1aa0c780b215f7303b1389c903444197de
gs.8life <gs.8life@gmail.com>
2024-08-09 12:11:03 +0900

    조선욱 : 타이머 업데이트

--- Source/Unreal5_Portfolio/PartDevLevel/UI/HUD/Timer_UserWidget.h
+++ Source/Unreal5_Portfolio/PartDevLevel/UI/HUD/Timer_UserWidget.h
@@ -17,8 +17,11 @@ class UNREAL5_PORTFOLIO_API UTimer_UserWidget : public UGetCurStage_UserWidget
 public:
 	UFUNCTION(BlueprintCallable)
 	void SetTimerCurStage(EGameStage _SetStage);
-	void SetTime(int _Hour,int _Min,int _Second);
-	void SetTime(int _Min, int _Second);
+	UFUNCTION(BlueprintCallable)
+	void SetTimer(int _Min, int _Second);
+	void SetTimer(int _Hour,int _Min,int _Second);
+
+
 	FText T_Time;
 	FTimespan NowTimeSpan;
 };
